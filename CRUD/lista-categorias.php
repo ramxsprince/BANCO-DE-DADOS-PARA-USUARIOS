96de40c930bda833495b5db40872dbbad0eb6114
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-01 10:13:57 -0300

    CATEGORIAS

--- CRUD/lista-categorias.php
+++ CRUD/lista-categorias.php
@@ -18,22 +18,25 @@ include_once './include/header.php';
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Dado A</td>
-
+            <?php
+          $sql = 'SELECT * FROM categorias'; 
+        $retorno = mysqli_query($conexao, $sql);
+        while ($linha = mysqli_fetch_assoc($retorno)) {
+          echo '    <tr>
+              <td>'.$linha['CategoriaID'].'</td>
+              <td>'.$linha['Nome'].'</td>
               <td>
-                <a href="#" class="btn btn-edit">Editar</a>
-                <a href="#" class="btn btn-delete">Excluir</a>
+                <a href="salvar-categorias.php?id='.$linha['CategoriaID'].'" class="btn btn-edit">Editar</a>
+                <a href="excluir-categorias.php?id='.$linha['CategoriaID'].'" class="btn btn-delete">Excluir</a>
               </td>
+                 </td>
             </tr>
-            <tr>
-              <td>2</td>
-              <td>Dado B</td>
-              <td>
-                <a href="#" class="btn btn-edit">Editar</a>
-                <a href="#" class="btn btn-delete">Excluir</a>
-              </td>
+           ';
+        }
+        ?>;
+
+
+
             </tr>
             
           </tbody>
