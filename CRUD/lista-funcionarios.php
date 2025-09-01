23dc3b49b10c8de2d51c40331ae66a2667c1d7da
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-01 10:11:12 -0300

    FUNCIONARIOS

--- CRUD/lista-funcionarios.php
+++ CRUD/lista-funcionarios.php
@@ -22,24 +22,26 @@ include_once './include/header.php';
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>Funcionário A</td>
-            <td>Cargo A</td>
-            <td>Setor A</td>
-            <td>
-              <a href="#" class="btn btn-edit">Editar</a>
-              <a href="#" class="btn btn-delete">Excluir</a>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Funcionário B</td>
-            <td>Cargo B</td>
-            <td>Setor B</td>
-            <td>
-              <a href="#" class="btn btn-edit">Editar</a>
-              <a href="#" class="btn btn-delete">Excluir</a>
-            </td>
+           <?php
+          $sql = 'SELECT f.FuncionarioID, f.Nome AS NomeFuncionario, c.Nome AS NomeCargo, s.Nome AS NomeSetor
+                  FROM funcionarios f
+                  JOIN cargos c ON f.CargoID = c.CargoID
+                  JOIN setor s ON f.SetorID = s.SetorID';
+        $retorno = mysqli_query($conexao, $sql);
+        while ($linha = mysqli_fetch_assoc($retorno)) {
+          echo '    <tr>
+              <td>'.$linha['FuncionarioID'].'</td>
+              <td>'.$linha['NomeFuncionario'].'</td>
+              <td>'.$linha['NomeCargo'].'</td>
+              <td>'.$linha['NomeSetor'].'</td>
+              <td>
+                <a href="salvar-funcionarios.php?id='.$linha['FuncionarioID'].'" class="btn btn-edit">Editar</a>
+                <a href="excluir-funcionarios.php?id='.$linha['FuncionarioID'].'" class="btn btn-delete">Excluir</a>
+              </td>
+            </tr>
+           ';
+        }
+        ?>;
           </tr>
           
         </tbody>
