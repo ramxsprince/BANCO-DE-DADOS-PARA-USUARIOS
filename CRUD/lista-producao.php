7db45513e75825871dbe09152b83e5bff749b2fc
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-04 08:37:13 -0300

    UPDATE LISTA PRODUÇÃO

--- CRUD/lista-producao.php
+++ CRUD/lista-producao.php
@@ -11,36 +11,36 @@ include_once './include/header.php';
         <a href="./salvar-producao.php" class="btn btn-add">Incluir</a> 
         <table>
           <thead>
-            <tr>
+            
+          <tr>
               <th>ID</th>
               <th>Produto</th>
-              <th>Quantidade</th>
+              <th>Produtor</th>
               <th>Data</th>
               <th>Ações</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>Produto A</td>
-              <td>100</td>
-              <td>2025-04-10</td>
+            <?php
+          $sql = 'SELECT p.ProducaoID, pr.Nome AS NomeProduto, f.Nome AS Nome, p.DataProducao
+                  FROM producao p
+                  JOIN produtos pr ON p.ProdutoID = pr.ProdutoID
+                  JOIN funcionarios f ON p.FuncionarioID = f.FuncionarioID';
+        $retorno = mysqli_query($conexao, $sql);
+        while ($linha = mysqli_fetch_assoc($retorno)) {
+          echo '    <tr>
+              <td>'.$linha['ProducaoID'].'</td>
+              <td>'.$linha['NomeProduto'].'</td>
+              <td>'.$linha['Nome'].'</td>
+              <td>'.$linha['DataProducao'].'</td>
               <td>
-                <a href="#" class="btn btn-edit">Editar</a>
-                <a href="#" class="btn btn-delete">Excluir</a>
+                <a href="salvar-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-edit">Editar</a>
+                <a href="excluir-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-delete">Excluir</a>
               </td>
             </tr>
-            <tr>
-              <td>2</td>
-              <td>Produto B</td>
-              <td>250</td>
-              <td>2025-04-12</td>
-              <td>
-                <a href="#" class="btn btn-edit">Editar</a>
-                <a href="#" class="btn btn-delete">Excluir</a>
-              </td>
-            </tr>
-            
+           ';
+        }
+        ?>;
           </tbody>
         </table>
       </div>
