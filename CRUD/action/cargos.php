9dd9022faaecb90eb43d8ee251fb00155bcaafbb
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-08 09:17:12 -0300

    INCLUIR PRODUCAO E EXCLUIR CARGOS

--- CRUD/action/cargos.php
+++ CRUD/action/cargos.php
@@ -8,8 +8,20 @@ $acao = $_GET['acao'];
 
 // validacao
 switch ($acao) {
-    case 'value':
-        # code...
+    case 'excluir':
+        $cargoID = $_GET['id'];
+        if (!empty($cargoID)) {
+            $sql = "DELETE FROM cargos WHERE CargoID = ?";
+            $stmt = mysqli_prepare($conexao, $sql);
+            mysqli_stmt_bind_param($stmt, 'i', $cargoID);
+            if (mysqli_stmt_execute($stmt)) {
+                header('Location: ../lista-cargos.php?msg=excluido');
+            } else {
+                header('Location: ../lista-cargos.php?msg=erro');
+            }
+        } else {
+            header('Location: ../lista-cargos.php?msg=invalid');
+        }
         break;
     
     default:
--- CRUD/salvar-producao.php
+++ CRUD/salvar-producao.php
@@ -3,17 +3,50 @@
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
+
+// Buscar funcionários, produtos e clientes do banco de dados
+$sqlFuncionarios = 'SELECT FuncionarioID, Nome FROM funcionarios';
+$resultadoFuncionarios = mysqli_query($conexao, $sqlFuncionarios);
+
+$sqlProdutos = 'SELECT ProdutoID, Nome FROM produtos';
+$resultadoProdutos = mysqli_query($conexao, $sqlProdutos);
+
+$sqlClientes = 'SELECT ClienteID, Nome FROM clientes';
+$resultadoClientes = mysqli_query($conexao, $sqlClientes);
 ?>
   <main>
 
     <div id="producao" class="tela">
-        <form class="crud-form" method="post" action="">
+        <form class="crud-form" method="post" action="salvar-producao.php">
           <h2>Cadastro de Produção de Produtos</h2>
-          <select>
-            <option value="">Funcionário</option>
+          <label for="funcionario">Funcionário:</label>
+          <select name="funcionario" id="funcionario" required>
+            <option value="">Selecione um funcionário</option>
+            <?php
+            while ($funcionario = mysqli_fetch_assoc($resultadoFuncionarios)) {
+              echo '<option value="'.$funcionario['FuncionarioID'].'">'.$funcionario['Nome'].'</option>';
+            }
+            ?>
           </select>
-          <select>
-            <option value="">Produto</option>
+
+          <label for="produto">Produto:</label>
+          <select name="produto" id="produto" required>
+            <option value="">Selecione um produto</option>
+            <?php
+            while ($produto = mysqli_fetch_assoc($resultadoProdutos)) {
+              echo '<option value="'.$produto['ProdutoID'].'">'.$produto['Nome'].'</option>';
+            }
+            ?>
+          </select>
+
+          <label for="cliente">Cliente:</label>
+          <select name="cliente" id="cliente" required>
+            <option value="">Selecione um cliente</option>
+            <?php
+            while ($cliente = mysqli_fetch_assoc($resultadoClientes)) {
+              echo '<option value="'.$cliente['ClienteID'].'">'.$cliente['Nome'].'</option>';
+            }
+            ?>
           </select>
           <label for="">Data da entrega</label>
           <input type="date" placeholder="Data da Entrega">
