be0fda621ff7a7a293b4746c44af2776ea4ad8ee
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-08 09:12:51 -0300

    Preencher os campos SELECT de Categoria no Incluir Produtos;

--- CRUD/salvar-produtos.php
+++ CRUD/salvar-produtos.php
@@ -3,19 +3,29 @@
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
+
+// Buscar categorias do banco de dados
+$sqlCategorias = 'SELECT CategoriaID, Nome FROM categorias';
+$resultadoCategorias = mysqli_query($conexao, $sqlCategorias);
 ?>
   
   <main>
 
     <div id="produtos" class="tela">
-        <form class="crud-form" action="" method="post">
+        <form class="crud-form" action="salvar-produtos.php" method="post">
           <h2>Cadastro de Produtos</h2>
           <input type="text" placeholder="Nome do Produto">
           <input type="number" placeholder="Preço">
           <input type="number" placeholder="Peso (g)">
           <textarea placeholder="Descrição"></textarea>
-          <select>
-            <option value="">Categoria</option>
+          <label for="categoria">Categoria:</label>
+          <select name="categoria" id="categoria" required>
+            <option value="">Selecione uma categoria</option>
+            <?php
+            while ($categoria = mysqli_fetch_assoc($resultadoCategorias)) {
+              echo '<option value="'.$categoria['CategoriaID'].'">'.$categoria['Nome'].'</option>';
+            }
+            ?>
           </select>
           <button type="submit">Salvar</button>
         </form>
