8851757e35a7284989c6d599a90f35b96ed8dc02
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-22 09:52:21 -0300

    Delete excluir_produto.php

--- excluir_produto.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conexao.php';
-
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $conn->query("DELETE FROM produtos WHERE id = $id");
-    header("Location: listar_produtos.php");
-}
-?>
