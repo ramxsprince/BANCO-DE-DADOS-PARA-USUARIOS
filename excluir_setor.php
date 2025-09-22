62a0a398514ec5d28513e25732f817fb728f0895
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-22 09:52:31 -0300

    Delete excluir_setor.php

--- excluir_setor.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conexao.php';
-
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $conn->query("DELETE FROM setores WHERE id = $id");
-    header("Location: listar_setores.php");
-}
-?> 
