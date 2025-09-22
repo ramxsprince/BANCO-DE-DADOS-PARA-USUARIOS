628d85b7c712e368801236ddf0e784e1fdcbe004
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-22 09:52:11 -0300

    Delete excluir_producao.php

--- excluir_producao.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'conexao.php';
-
-if (isset($_GET['id'])) {
-    $id = intval($_GET['id']);
-    $conn->query("DELETE FROM producao WHERE id = $id");
-    header("Location: listar_producao.php");
-}
-?>
