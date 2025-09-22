4897671e4e1df172a393b2eea227786fca8563ae
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-22 09:49:41 -0300

    LISTA PRODUCAO FUNCIONANDO

--- CRUD/lista-producao.php
+++ CRUD/lista-producao.php
@@ -1,20 +1,19 @@
-<?php 
+<?php
 // include dos arquivos
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
 ?>
   <main>
-
+ 
     <div class="container">
         <h1>Lista de Produções</h1>
-        <a href="./salvar-producao.php" class="btn btn-add">Incluir</a> 
+        <a href="./salvar-producao.php" class="btn btn-add">Incluir</a>
         <table>
           <thead>
             <tr>
               <th>ID</th>
               <th>Produto</th>
-              <th>Cliente</th>
               <th>Funcionário</th>
               <th>Data</th>
               <th>Ações</th>
@@ -22,35 +21,39 @@ include_once './include/header.php';
           </thead>
           <tbody>
           <?php
-          $sql = 'SELECT ProducaoID, c.Nome AS NomeCliente, DataProducao, f.Nome AS NomeFunc, p.Nome AS NomeProduto FROM producao
-          INNER JOIN produtos AS p ON producao.ProdutoID = p.ProdutoID
-          INNER JOIN funcionarios AS f ON producao.FuncionarioID = f.FuncionarioID
-          INNER JOIN clientes AS c ON producao.ClienteID = c.ClienteID
-          ORDER BY ProducaoID ASC;';
-
-          $return = mysqli_query($conexao, $sql);
-
-          while ($linha = mysqli_fetch_assoc($return)) {
-            $dataFormatada = date('d/m/Y', strtotime($linha['DataProducao']));
-            echo '<tr id="'.$linha['ProducaoID'].'">
-              <td>'.$linha['ProducaoID'].'</td>
-              <td>'.$linha['NomeProduto'].'</td>
-              <td>'.$linha['NomeCliente'].'</td>
-              <td>'.$linha['NomeFunc'].'</td>
-              <td>'.$dataFormatada.'</td>
-              <td>
-                <a href="./salvar-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-edit">Editar</a>
-                <a href="./action/producao.php?id='.$linha['ProducaoID'].'&acao=excluir" class="btn btn-delete">Excluir</a>
-              </td>
-            </tr>';
+          // PEGAR NOME DO produto e NOME DO funcionario
+          $sql = 'SELECT p.ProducaoID, pr.Nome AS NomeProduto, f.Nome AS Nome, p.DataProducao
+                  FROM producao p 
+                  LEFT JOIN produtos pr ON p.ProdutoID = pr.ProdutoID
+                  LEFT JOIN funcionarios f ON p.FuncionarioID = f.FuncionarioID';
+          $resultado = mysqli_query($conexao, $sql);
+          if (mysqli_num_rows($resultado) > 0) {
+            while($row = mysqli_fetch_assoc($resultado)) {
+              echo "<tr>
+                      <td>".$row["ProducaoID"]."</td>
+                      <td>".$row["NomeProduto"]."</td>
+                      <td>".$row["Nome"]."</td>
+                      <td>".$row["DataProducao"]."</td>
+                      <td>
+                        <a href='salvar-producao.php?id=".$row["ProducaoID"]."' class='btn btn-edit'>Editar</a>
+                        <a href='./action/producao.php?acao=excluir&id=".$row["ProducaoID"]."' class='btn btn-delete'>Excluir</a>
+                      </td>
+                    </tr>";
+            }
+          } else {
+            echo "<tr><td colspan='4'>Nenhuma produção encontrada.</td></tr>";
           }
           ?>
           </tbody>
         </table>
       </div>
+ 
+ 
+   
   </main>
-
-  <?php 
+ 
+  <?php
   // include dos arquivos
   include_once './include/footer.php';
   ?>
+ 
