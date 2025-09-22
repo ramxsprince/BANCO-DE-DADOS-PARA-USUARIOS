48ead0010df14964f840fab0fb6a6c60459753bd
Cassiano Ramos <cassianolori5@gmail.com>
2025-09-22 08:58:53 -0300

    .

--- CRUD/lista-producao.php
+++ CRUD/lista-producao.php
@@ -1,5 +1,5 @@
 <?php 
-// include dos arquivos
+// include dos arquivox
 include_once './include/logado.php';
 include_once './include/conexao.php';
 include_once './include/header.php';
@@ -11,46 +11,45 @@ include_once './include/header.php';
         <a href="./salvar-producao.php" class="btn btn-add">Incluir</a> 
         <table>
           <thead>
-            <tr>
+            
+          <tr>
               <th>ID</th>
               <th>Produto</th>
-              <th>Cliente</th>
-              <th>Funcionário</th>
+              <th>Produtor</th>
               <th>Data</th>
               <th>Ações</th>
             </tr>
           </thead>
           <tbody>
-          <?php
-          $sql = 'SELECT ProducaoID, c.Nome AS NomeCliente, DataProducao, f.Nome AS NomeFunc, p.Nome AS NomeProduto FROM producao
-          INNER JOIN produtos AS p ON producao.ProdutoID = p.ProdutoID
-          INNER JOIN funcionarios AS f ON producao.FuncionarioID = f.FuncionarioID
-          INNER JOIN clientes AS c ON producao.ClienteID = c.ClienteID
-          ORDER BY ProducaoID ASC;';
-
-          $return = mysqli_query($conexao, $sql);
-
-          while ($linha = mysqli_fetch_assoc($return)) {
-            $dataFormatada = date('d/m/Y', strtotime($linha['DataProducao']));
-            echo '<tr id="'.$linha['ProducaoID'].'">
+            <?php
+          $sql = 'SELECT p.ProducaoID, pr.Nome AS NomeProduto, f.Nome AS Nome, p.DataProducao
+                  FROM producao p
+                  JOIN produtos pr ON p.ProdutoID = pr.ProdutoID
+                  JOIN funcionarios f ON p.FuncionarioID = f.FuncionarioID';
+        $retorno = mysqli_query($conexao, $sql);
+        while ($linha = mysqli_fetch_assoc($retorno)) {
+          echo '    <tr>
               <td>'.$linha['ProducaoID'].'</td>
               <td>'.$linha['NomeProduto'].'</td>
-              <td>'.$linha['NomeCliente'].'</td>
-              <td>'.$linha['NomeFunc'].'</td>
-              <td>'.$dataFormatada.'</td>
+              <td>'.$linha['Nome'].'</td>
+              <td>'.$linha['DataProducao'].'</td>
               <td>
-                <a href="./salvar-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-edit">Editar</a>
-                <a href="./action/producao.php?id='.$linha['ProducaoID'].'&acao=excluir" class="btn btn-delete">Excluir</a>
+                <a href="salvar-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-edit">Editar</a>
+                <a href="excluir-producao.php?id='.$linha['ProducaoID'].'" class="btn btn-delete">Excluir</a>
               </td>
-            </tr>';
-          }
-          ?>
+            </tr>
+           ';
+        }
+        ?>;
           </tbody>
         </table>
       </div>
+
+
+   
   </main>
 
   <?php 
-  // include dos arquivos
+  // include dos arquivox
   include_once './include/footer.php';
   ?>
